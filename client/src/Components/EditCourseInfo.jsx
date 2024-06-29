07c50f5ce6df80913fd02010162ad4fd6a3e3a1f
Nick Hoefle <Nickhoefle@gmail.com>
2024-06-28 22:48:14 -0500

    Removed option to edit hole amount; added code for adding/removing teeboxes.

--- client/src/Components/EditCourseInfo.jsx
+++ client/src/Components/EditCourseInfo.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const EditCourseInfo = () => {
-    const [courseInfo, setCourseInfo] = useState({});
+    const [initialCourseData, setInitialCourseData] = useState({});
     const [formData, setFormData] = useState({
         name: '',
         location: '',
@@ -12,43 +12,72 @@ const EditCourseInfo = () => {
     });
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchCourseData = async () => {
             const queryParams = new URLSearchParams(window.location.search);
             const courseId = queryParams.get('id'); 
             try {
                 if (courseId) {
                     const response = await axios.get(`/api/golf-courses/${courseId}`);
-                    setCourseInfo(response.data)
+                    setInitialCourseData(response.data);
                     setFormData({
                         name: response.data.name,
                         location: response.data.location,
                         holes: response.data.holes.toString(),
                         pars: response.data.pars || [],
-                        teeBoxes: response.data.teeBoxes,
+                        teeBoxes: response.data.teeBoxes || [],
                     });
                 }
             } catch (error) {
-                console.error('Error fetching golf outings:', error);
+                console.error('Error fetching course data:', error);
             }
         };
     
-        fetchData();
+        fetchCourseData();
     }, []); 
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
+    
         if (name === 'holes') {
             const newPars = formData.pars.slice(0, parseInt(value));
-            setFormData({ ...formData, [name]: value, pars: newPars });
+            const updatedTeeBoxes = formData.teeBoxes.map(teeBox => {
+                const updatedYardages = teeBox.yardages.slice(0, parseInt(value));
+                return { ...teeBox, yardages: updatedYardages };
+            });
+    
+            setFormData({ ...formData, [name]: value, pars: newPars, teeBoxes: updatedTeeBoxes });
         } else {
             setFormData({ ...formData, [name]: value });
+        }
+    };
+
+    const handleAddTeeBox = () => {
+        const newTeeBox = {
+            color: '',
+            yardages: Array.from({ length: parseInt(formData.holes) }).fill(''),
+        };
+
+        setFormData(prevState => ({
+            ...prevState,
+            teeBoxes: [...prevState.teeBoxes, newTeeBox],
+        }));
+    };
+
+    const handleDeleteTeeBox = (index) => {
+        const confirmation = window.confirm('Are you sure you want to delete this tee box?');
+        if (confirmation) {
+            setFormData(prevState => {
+                const updatedTeeBoxes = [...prevState.teeBoxes];
+                updatedTeeBoxes.splice(index, 1);
+                return { ...prevState, teeBoxes: updatedTeeBoxes };
+            });
         }
     };
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         try {
-            const courseId = courseInfo._id; 
+            const courseId = initialCourseData._id; 
             const response = await axios.put(`/api/golf-courses/${courseId}`, formData);
             console.log('Course updated:', response.data);
         } catch (error) {
@@ -79,14 +108,7 @@ const EditCourseInfo = () => {
                         onChange={handleInputChange}
                     />
                     <label>Holes:</label>
-                    <select
-                        value={formData.holes}
-                        onChange={handleInputChange}
-                        name="holes"
-                    >
-                        <option value='9'>9</option>
-                        <option value='18'>18</option>
-                    </select>
+                    <span>{formData.holes}</span>
                     <div className='edit-pars-wrapper'>
                         {Array.from({ length: parseInt(formData.holes) }).map((_, holeIndex) => (
                             <div key={holeIndex} className='edit-par-line'>
@@ -106,42 +128,50 @@ const EditCourseInfo = () => {
                     </div>
                     <div>
                         {formData.teeBoxes.map((teeBox, index) => (
-                            <div key={index}>
-                                <label>Tee Box #{index+1}</label>
-                                <select
-                                    value={teeBox.color}
-                                    onChange={(e) => {
-                                        const newTeeBoxColor = [...formData.teeBoxes];
-                                        newTeeBoxColor[index].color = e.target.value;
-                                        setFormData({ ...formData, teeBoxes: newTeeBoxColor });
-                                    }}
-                                >
-                                    <option value="">Select Color</option>
-                                    <option value="Red">Red</option>
-                                    <option value="Orange">Orange</option>
-                                    <option value="Yellow">Yellow</option>
-                                    <option value="Green">Green</option>
-                                    <option value="Blue">Blue</option>
-                                    <option value="Purple">Violet</option>
-                                    <option value="White">White</option>
-                                    <option value="Black">Black</option>
-                                    <option value="Brown">Brown</option>
-                                    <option value="Gray">Gray</option>
-                                    <option value="Pink">Pink</option>
-                                </select>
-                                {formData.teeBoxes[index].yardages.map((yardage, holeIndex) => (
-                                    <input 
-                                        key={holeIndex}
-                                        value={yardage}
+                            <div 
+                                key={index}
+                                className='edit-teebox-wrapper'
+                            >
+                                <div>
+                                    <label>Tee Box #{index+1}</label>
+                                    <select
+                                        value={teeBox.color}
                                         onChange={(e) => {
-                                            const newTeeBoxYardages = [...formData.teeBoxes];
-                                            newTeeBoxYardages[index].yardages[holeIndex] = e.target.value;
-                                            setFormData({ ...formData, teeBoxes: newTeeBoxYardages });
+                                            const newTeeBoxColor = [...formData.teeBoxes];
+                                            newTeeBoxColor[index].color = e.target.value;
+                                            setFormData({ ...formData, teeBoxes: newTeeBoxColor });
                                         }}
-                                    />
-                                ))} 
+                                    >
+                                        <option value="">Select Color</option>
+                                        <option value="Red">Red</option>
+                                        <option value="Orange">Orange</option>
+                                        <option value="Yellow">Yellow</option>
+                                        <option value="Green">Green</option>
+                                        <option value="Blue">Blue</option>
+                                        <option value="Purple">Violet</option>
+                                        <option value="White">White</option>
+                                        <option value="Black">Black</option>
+                                        <option value="Brown">Brown</option>
+                                        <option value="Gray">Gray</option>
+                                        <option value="Pink">Pink</option>
+                                    </select>
+                                    {teeBox.yardages.map((yardage, holeIndex) => (
+                                        <input 
+                                            required
+                                            key={holeIndex}
+                                            value={yardage}
+                                            onChange={(e) => {
+                                                const newTeeBoxYardages = [...formData.teeBoxes];
+                                                newTeeBoxYardages[index].yardages[holeIndex] = e.target.value;
+                                                setFormData({ ...formData, teeBoxes: newTeeBoxYardages });
+                                            }}
+                                        />
+                                    ))}
+                                    <button type="button" onClick={() => handleDeleteTeeBox(index)}>Delete</button>
+                                </div>
                             </div>
-                        ))} 
+                        ))}
+                        <button type="button" onClick={handleAddTeeBox}>Add Tee Box</button>
                     </div>
                     <button type="submit">Update Course</button>
                 </form>
