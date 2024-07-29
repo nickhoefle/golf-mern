d581ab4f30d9d3da02ed4a86f852f81be1bb5770
Nick Hoefle <Nickhoefle@gmail.com>
2024-07-28 20:30:54 -0500

    Styled EditGolfOuting.jsx view and added label for course name.

--- client/src/Components/EditGolfOuting.jsx
+++ client/src/Components/EditGolfOuting.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const EditGolfOuting = () => {
+    const [courseName, setCourseName] = useState('');
     const [outingData, setOutingData] = useState(null);
     const [formData, setFormData] = useState({
         user: '',
@@ -37,6 +38,18 @@ const EditGolfOuting = () => {
         fetchOutingData();
     }, [outingId]);
 
+    useEffect(() => {
+        const fetchCourseById = async (courseId) => {
+            try {
+                const response = await axios.get(`/api/golf-courses/${courseId}`);
+                setCourseName(response.data.name);
+            } catch (error) {
+                console.error('Error fetching outing data:', error);
+            }
+        };
+        fetchCourseById(formData.course);
+    }, [formData.course]);
+
     const handleScoreChange = (index, value) => {
         const newScores = [...formData.scores];
         const score = parseInt(value);
@@ -62,6 +75,7 @@ const EditGolfOuting = () => {
             const response = await axios.put(`/api/golf-outings/${outingId}`, formData, {
                 headers: { 'Content-Type': 'application/json' }             
             });
+            console.log(response);
             window.location.href = `/?id=${formData.course}`
         } catch (error) {
             console.error('Error updating golf outing:', error.message);
@@ -72,37 +86,42 @@ const EditGolfOuting = () => {
         return <div>Loading...</div>;
     }
 
+    console.log(formData.scores.length)
+
     return (
-        <div>
-            <h1>Edit Golf Outing</h1>
+        <div className='edit-golf-outing-wrapper'>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>User:</label>
+                <h1>Edit Golf Outing</h1>
+                <label>User:</label>
+                <input
+                    className='edit-golf-outing-email'
+                    type="text"
+                    value={formData.user}
+                />
+                <label>Date:</label>
                     <input
-                        type="text"
-                        value={formData.user}
+                        style={{ padding: formData.scores.length === 18 ? '20px 0 20px 5px' : '5px' }}
+                        className='edit-golf-outing-date'
+                        type="date"
+                        value={formData.date}
+                        onChange={handleDateChange}
                     />
-                </div>
-                <div>
-                    <label>Scores:</label>
-                    {formData.scores.map((score, index) => (
+                    <label>Course:</label>
+                    <span className='edit-golf-outing-course-name'>{courseName}</span>
+                {formData.scores.map((score, index) => (
+                    <div>
+                        <label>Hole {index + 1} Score: </label>
                         <input
+                            className='edit-golf-outing-score'
                             key={index}
                             type="number"
                             value={score || ''} 
                             onChange={(e) => handleScoreChange(index, e.target.value)}
                             min="0" 
+                            style={{ marginLeft: index < 9 ? '20px' : '8px' }}
                         />
-                    ))}
-                </div>
-                <div>
-                    <label>Date:</label>
-                    <input
-                        type="date"
-                        value={formData.date}
-                        onChange={handleDateChange}
-                    />
-                </div>
+                    </div>
+                ))}
                 <button type="submit" className='submit-golf-outing-button'>
                     Submit
                 </button>
